dba59243f24d9db96d1f217221d5872b7962e7f2
91844 <91844@LAPTOP-6R0B90S7>
2025-01-07 17:33:11 +0530

    stage3

--- DecBatch2024/src/decBatch/testing1.java
+++ DecBatch2024/src/decBatch/testing1.java
@@ -1,10 +1,14 @@
 package decBatch;
 
 public class testing1 {
-
+int c=55;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		testing1 tst1=new testing1();
+	System.out.println("The variable value:" +tst1.c);
 
+		
+		
+		
 	}
 
 }
