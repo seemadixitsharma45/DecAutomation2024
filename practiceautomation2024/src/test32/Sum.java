d497a7e00e960d278269186d946756603c799162
91844 <91844@LAPTOP-6R0B90S7>
2025-01-07 18:38:59 +0530

    Test1

---
+++ practiceautomation2024/src/test32/Sum.java
@@ -0,0 +1,25 @@
+package test32;
+
+public class Sum {
+	
+	String s="Seema";
+	int id=12345;
+			
+			
+			
+			
+		public void display()
+		{
+			System.out.println("Name of candidate:" +s+ " and id :"+id );
+		}
+	
+	
+	
+
+	public static void main(String[] args) {
+		Sum gst=new Sum();
+		gst.display();
+
+	}
+
+}
