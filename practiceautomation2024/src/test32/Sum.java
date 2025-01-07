eeda33c4159ca183b51b874a8bd53cc4587711f0
91844 <91844@LAPTOP-6R0B90S7>
2025-01-07 18:48:37 +0530

    test2

--- practiceautomation2024/src/test32/Sum.java
+++ practiceautomation2024/src/test32/Sum.java
@@ -5,7 +5,7 @@ public class Sum {
 	String s="Seema";
 	int id=12345;
 			
-			
+		 String batch="2024";	
 			
 			
 		public void display()
@@ -19,7 +19,7 @@ public class Sum {
 	public static void main(String[] args) {
 		Sum gst=new Sum();
 		gst.display();
-
+System.out.println(gst.batch);
 	}
 
 }
